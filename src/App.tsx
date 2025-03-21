4c573d1de14ff21d33f72b584253833d85883421
Simon Conrad <simondbconrad@gmail.com>
2025-03-20 23:00:57 -0400

    render basic map

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import { useRef, useEffect } from 'react'
+import mapboxgl, {Map} from 'mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
+
 import './App.css'
 
 function App() {
+  const mapboxToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN
+  const mapRef = useRef<Map|null>(null)
+  const mapContainerRef = useRef<HTMLDivElement|null>(null)
+
+  useEffect(() => {
+    if (!mapContainerRef.current){
+      return
+    }
+    mapboxgl.accessToken = mapboxToken
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+    });
+
+    return () => {
+      mapRef.current?.remove()
+    }
+  }, [mapboxToken])
 
   return (
     <>
-      <div id="map-container"></div>
+      <div id="map-container" ref={mapContainerRef}></div>
     </>
   )
 }
