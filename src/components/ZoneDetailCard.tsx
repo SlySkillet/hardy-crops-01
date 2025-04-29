189c015cc23fdf9e781cb5c7e531f89d837608bc
Simon Conrad <simondbconrad@gmail.com>
2025-04-29 17:08:58 -0400

    additional zone data

--- src/components/ZoneDetailCard.tsx
+++ src/components/ZoneDetailCard.tsx
@@ -13,12 +13,11 @@ interface ZoneDetailCardProps {
 }
 
 const ZoneDetailCard: React.FC<ZoneDetailCardProps> = ({ zone }) => {
-    // zone title is more specific in attribute table than current descriptions require. Stripping zone title to more general number eg. '6' instead of '6a'
+  // zone title is more specific in attribute table than current descriptions require. Stripping zone title to more general number eg. '6' instead of '6a'
   const zoneStripped =
     (zone?.zone ?? "").length < 3
       ? zone?.zone.slice(0, 1)
       : zone?.zone.slice(0, 2);
-
 
   // Helper function to truncate CropData by selected zone
   const renderZoneDescription = (zoneString: keyof typeof CropData) => {
@@ -31,10 +30,12 @@ const ZoneDetailCard: React.FC<ZoneDetailCardProps> = ({ zone }) => {
 
   return (
     <div className=" overflow-auto w-96 flex flex-col bg-stone-800 text-stone-100 p-3">
-      <h1 className="font-bold text-2xl">USDA Hardiness Zones</h1>
+      <h1 className="font-bold text-3xl">USDA Hardiness Zones</h1>
       {zone ? (
         <div>
-          <h2>{zone.zone}</h2>
+          <h2 className="font-bold text-2xl">Zone {zone.zone}</h2>
+          <h5 className="font-bold">Average Anual Extreme Temperature:</h5>
+          <p>{zone?.trange} &deg;F</p>
           <div className="text-left text-sm">
             {data?.map((item: string | Array<string>, idx: number) => {
               return typeof item === "string" ? (
